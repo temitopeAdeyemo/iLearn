96a848d0190988e6a46dea1e8ba4d73421634b27
temitopeAdeyemo <temitopejulius9@gmail.com>
2024-09-17 14:14:54 +0100

    Init codebase

---
+++ src/main/java/com/backend/iLearn/ILearnApplication.java
@@ -0,0 +1,13 @@
+package com.backend.iLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/iLearn/ILearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.iLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
