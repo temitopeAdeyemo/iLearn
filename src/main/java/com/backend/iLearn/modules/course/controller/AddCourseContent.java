3b12cf9a11104027976c99f210b4a5e27776acbf
temitopeAdeyemo <temitopejulius9@gmail.com>
2025-05-31 11:22:24 +0100

    Refactored and tested code

--- src/main/java/com/backend/iLearn/modules/course/controller/AddCourseContent.java
+++ src/main/java/com/backend/iLearn/modules/course/controller/AddCourseContent.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/video/course")
+@RequestMapping("/api/v1/course/content")
 @RequiredArgsConstructor
 public class AddCourseContent {
     private final CreateCourseContentService createCourseContentService;
